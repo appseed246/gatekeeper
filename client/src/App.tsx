f4cae5419e890ad859b9bcbead36f54183787c47
appseed246 <guitar.telecaster2017@gmail.com>
2019-10-15 01:26:45 +0900

    サンプルコード作成

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import axios from "axios";
+
+type ProductResponse = {
+  id: number;
+  product: string;
+  created_at: Date;
+  updated_at: Date;
+};
+
+const target_url: string = "http://localhost:3001/products";
+
+const useResponse = () => {
+  const fetchResource = async () => {
+    const response = await axios.get<ProductResponse[]>(target_url);
+    return response.data;
+  };
+  const [data, setData] = useState<ProductResponse[]>([]);
+  useEffect(() => {
+    fetchResource().then(res => setData(res));
+  }, []);
+
+  return data;
+};
 
 const App: React.FC = () => {
+  const products = useResponse();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello, world to Gatekeeper App!</p>
+        <ul>
+          {products.map(product => {
+            return <li>{product.product}</li>;
+          })}
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ server/db/migrate/20191012132955_create_products.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.text :product
+      t.timestamps
+    end
+  end
+end
