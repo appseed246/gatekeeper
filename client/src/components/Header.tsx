f44eddafa9de0a751498be8dd3e89945f83fd600
appseed246 <guitar.telecaster2017@gmail.com>
2019-11-29 00:16:59 +0900

    メニュードロワー追加

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -6,9 +6,22 @@ import {
   createStyles,
   makeStyles,
   Typography,
-  Theme
+  Theme,
+  useTheme,
+  Divider,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Hidden,
+  Drawer
 } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
+import InboxIcon from "@material-ui/icons/Inbox";
+import MailIcon from "@material-ui/icons/Mail";
+import CustomPaginationActionsTable from "./CustomPaginationActionsTable";
+
+const drawerWidth = 240;
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -20,20 +33,71 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     title: {
       flexGrow: 1
+    },
+    // for drawer
+    drawer: {
+      [theme.breakpoints.up("sm")]: {
+        width: drawerWidth,
+        flexShrink: 0
+      }
+    },
+    toolbar: theme.mixins.toolbar,
+    drawerPaper: {
+      width: drawerWidth
     }
   })
 );
 
-export default function Header() {
+interface HeaderProps {
+  container?: Element;
+}
+
+export default function Header(props: HeaderProps) {
+  const { container } = props;
   const classes = useStyles();
+  const theme = useTheme();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <div>
+      <div className={classes.toolbar} />
+      <Divider />
+      <List>
+        {["Inbox", "Starred", "Send Mail", "Drafts"].map((text, index) => (
+          <ListItem>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
 
   return (
     <div className={classes.root}>
-      <AppBar position="sticky">
+      <AppBar position="fixed">
         <Toolbar>
           <IconButton
             edge="start"
             className={classes.menuButton}
+            onClick={handleDrawerToggle}
             color="inherit"
             aria-label="menu"
           >
@@ -44,6 +108,24 @@ export default function Header() {
           </Typography>
         </Toolbar>
       </AppBar>
+      {/* Drawer Section */}
+      <nav className={classes.drawer} aria-label="mailbox folders">
+        <Drawer
+          container={container}
+          variant="temporary"
+          anchor={theme.direction === "rtl" ? "right" : "left"}
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          classes={{
+            paper: classes.drawerPaper
+          }}
+          ModalProps={{
+            keepMounted: true
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
     </div>
   );
 }
